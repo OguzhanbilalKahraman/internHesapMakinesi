71632b49fb726e61fc0c8aae8f8b105cc7d7960c
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-08 11:50:34 +0300

    Add files via upload

---
+++ hesapMakinesi/hesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace hesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTop = new System.Windows.Forms.Button();
+            this.btnCik = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(326, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(326, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number 2";
+            // 
+            // btnTop
+            // 
+            this.btnTop.Location = new System.Drawing.Point(263, 285);
+            this.btnTop.Name = "btnTop";
+            this.btnTop.Size = new System.Drawing.Size(75, 23);
+            this.btnTop.TabIndex = 2;
+            this.btnTop.Text = "+";
+            this.btnTop.UseVisualStyleBackColor = true;
+            this.btnTop.Click += new System.EventHandler(this.btnTop_Click);
+            // 
+            // btnCik
+            // 
+            this.btnCik.Location = new System.Drawing.Point(389, 285);
+            this.btnCik.Name = "btnCik";
+            this.btnCik.Size = new System.Drawing.Size(75, 23);
+            this.btnCik.TabIndex = 2;
+            this.btnCik.Text = "-";
+            this.btnCik.UseVisualStyleBackColor = true;
+            this.btnCik.Click += new System.EventHandler(this.btnCik_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Location = new System.Drawing.Point(263, 337);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(75, 23);
+            this.btnBol.TabIndex = 2;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Location = new System.Drawing.Point(389, 337);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(75, 23);
+            this.btnCarp.TabIndex = 2;
+            this.btnCarp.Text = "x";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCik);
+            this.Controls.Add(this.btnTop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTop;
+        private System.Windows.Forms.Button btnCik;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Button btnCarp;
+    }
+}
+
---
+++ hesapMakinesi/hesapMakinesi/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double number1;
+        double number2;
+        double result;
+        
+
+        private void btnTop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number1 = Convert.ToDouble(textBox1.Text);
+                number2 = Convert.ToDouble(textBox2.Text);
+                result = number1 + number2;
+                MessageBox.Show("Result is: " + result.ToString());
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Pls only write number!");
+            }
+
+        }
+
+        private void btnCik_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number1 = Convert.ToDouble(textBox1.Text);
+                number2 = Convert.ToDouble(textBox2.Text);
+                result = number1 - number2;
+                MessageBox.Show("Result is: " + result.ToString());
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Pls only write number!");
+            }
+
+        }
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number1 = Convert.ToDouble(textBox1.Text);
+                number2 = Convert.ToDouble(textBox2.Text);
+                result = number1 / number2;
+                MessageBox.Show("Result is: "+result.ToString());
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Pls only write number!");
+
+            }
+
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                number1 = Convert.ToDouble(textBox1.Text);
+                number2 = Convert.ToDouble(textBox2.Text);
+                result = number1 * number2;
+                MessageBox.Show("Result is: " + result.ToString());
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Pls only write number!");
+            }
+
+        }
+    }
+}
